8ff3c8f6f335f129991f1c94859266e003f386e7
Kishan <dracx.py@gmail.com>
2024-11-21 23:01:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jsnDB',
-    version='1.0.0',
+    version='1.0.2',
     packages=find_packages(),
     description='A lightweight JSON-based database.',
     long_description=open('README.md').read(),
